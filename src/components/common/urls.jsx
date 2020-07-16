5fef768a0e5a93bc7dd21899d3632637dbe5392d
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2020-07-16 16:12:36 +0530

    Update urls.jsx

--- src/components/common/urls.jsx
+++ src/components/common/urls.jsx
@@ -1,2 +1,2 @@
-export const Data = 'http://localhost:8080/data'
-export const Auth = 'http://localhost:8080/auth'
+export const Data = 'https://floating-wave-16535.herokuapp.com/data'
+export const Auth = 'https://floating-wave-16535.herokuapp.com/auth'
